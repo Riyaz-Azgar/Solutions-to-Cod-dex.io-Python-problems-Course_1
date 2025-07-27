05272242089b1dac39630a0ea2705031952fbb3e
Ali Azgar Shaik <riyazazgar1@gmail.com>
2025-07-27 22:26:05 +0530

    Add files via upload

---
+++ 21. FizzBuzz.py
@@ -0,0 +1,9 @@
+for r in range(1, 101):  
+    if r % 3 == 0 and r % 5 == 0:
+        print("FizzBuzz")
+    elif r % 3 == 0:
+        print("Fizz")
+    elif r % 5 == 0:
+        print("Buzz")
+    else:
+        print(r)
